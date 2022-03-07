bd2cf5fa27ef2d98aca5421bbe32ee12866552f9
김현수 <hskim9706@gmail.com>
2022-03-07 17:49:14 +0900

    [S06P22D205-67]feat : :sparkles: Activity, Fragment 생성 및 viewpager 설정

--- Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ssafy.nooni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.ssafy.nooni.adapter.ViewpagerFragmentAdapter
+import com.ssafy.nooni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewpager: ViewPager2 = binding.viewpager
+        val viewpagerFragmentAdapter = ViewpagerFragmentAdapter(this)
+
+        viewpager.adapter = viewpagerFragmentAdapter
+        viewpager.currentItem = 1
     }
 }
