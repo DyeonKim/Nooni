f3f40b06b315c53b8a5ffaaa2226086b1f0e69fc
김현수 <hskim9706@gmail.com>
2022-03-07 17:47:54 +0900

    [S06P22D205-67]feat : :sparkles: ViewpagerAdapter 구현

---
+++ Nooni/app/src/main/java/com/ssafy/nooni/adapter/ViewpagerFragmentAdapter.kt
@@ -0,0 +1,23 @@
+package com.ssafy.nooni.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.ssafy.nooni.AllergyFragment
+import com.ssafy.nooni.CameraFragment
+import com.ssafy.nooni.ContactFragment
+
+class ViewpagerFragmentAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    // 1. ViewPager2에 연결할 Fragment 들을 생성
+    val fragmentList = listOf<Fragment>(AllergyFragment(), CameraFragment(), ContactFragment())
+
+    // 2. ViewPager2에서 노출시킬 Fragment 의 갯수 설정
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    // 3. ViewPager2의 각 페이지에서 노출할 Fragment 설정
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
