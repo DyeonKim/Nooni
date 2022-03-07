ab4bb09eedcafca765e6bbf0d25be0c6e42365b4
김현수 <hskim9706@gmail.com>
2022-03-08 00:57:43 +0900

    [S06P22D205-63]feat : :sparkles: MainActivity에서 권한 체크

--- Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ssafy.nooni
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
@@ -7,20 +8,46 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.ssafy.nooni.adapter.ViewpagerFragmentAdapter
 import com.ssafy.nooni.databinding.ActivityMainBinding
+import com.ssafy.nooni.util.PermissionUtil
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var permissionUtil: PermissionUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        permissionUtil = PermissionUtil(this)
+        permissionUtil.permissionListener = object : PermissionUtil.PermissionListener {
+            override fun run() {
+                init()
+            }
+        }
+    }
+
+    private fun init() {
         val viewpager: ViewPager2 = binding.viewpager
         val viewpagerFragmentAdapter = ViewpagerFragmentAdapter(this)
 
         viewpager.adapter = viewpagerFragmentAdapter
         viewpager.currentItem = 1
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        if(!permissionUtil.checkPermissions(listOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,))) {
+            permissionUtil.requestPermissions()
+        } else {
+            init()
+        }
     }
 }
