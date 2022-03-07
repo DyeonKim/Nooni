b4a4ea1eb32c00c9f9af24cb953a33c44b39a05c
김현수 <hskim9706@gmail.com>
2022-03-08 04:11:36 +0900

    [S06P22D205-65]feat : :sparkles: 알러지 등록 액티비티 추가

---
+++ Nooni/app/src/main/java/com/ssafy/nooni/RegisterAllergyActivity.kt
@@ -0,0 +1,11 @@
+package com.ssafy.nooni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterAllergyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_allergy)
+    }
+}
