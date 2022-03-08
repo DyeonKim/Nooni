b053e199e5ce00a78b4e4ed334ba4ad84fb3131c
김현수 <hskim9706@gmail.com>
2022-03-08 11:07:27 +0900

    [S06P22D205-66]feat : :sparkles: 연락처 접근권한 추가

--- Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
@@ -44,7 +44,8 @@ class MainActivity : AppCompatActivity() {
     private fun checkPermissions() {
         if(!permissionUtil.checkPermissions(listOf(
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA,))) {
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_CONTACTS))) {
             permissionUtil.requestPermissions()
         } else {
             init()
