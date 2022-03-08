5d3d43d8a3b5efcdbcf817f09d9f1c27b7bb0fe7
김현수 <hskim9706@gmail.com>
2022-03-08 13:50:26 +0900

    [S06P22D205-66]feat : :sparkles: Contact entity 생성

---
+++ Nooni/app/src/main/java/com/ssafy/nooni/entity/Contact.kt
@@ -0,0 +1,14 @@
+package com.ssafy.nooni.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Contact (
+    var name: String,
+    var phone: String,
+    var photo_id: Long,
+    var person_id: Long
+){
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
