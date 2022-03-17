ade005fc65c33aa39b930713c8d80cda51d4f3ba
김현수 <hskim9706@gmail.com>
2022-03-17 14:42:54 +0900

    [S06P22D205-70]feat : ✨ 알레르기 등록 화면 tts

--- Nooni/app/src/main/java/com/ssafy/nooni/RegisterAllergyActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/RegisterAllergyActivity.kt
@@ -2,14 +2,19 @@ package com.ssafy.nooni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.ERROR
 import android.util.Log
 import android.widget.Toast
 import com.ssafy.nooni.databinding.ActivityRegisterAllergyBinding
 import com.ssafy.nooni.util.SharedPrefArrayListUtil
+import java.util.*
+import kotlin.collections.ArrayList
 
 private const val TAG = "RegisterAllergy"
 class RegisterAllergyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterAllergyBinding
+    private lateinit var tts2: TextToSpeech
     var sharePrefArrayListUtil = SharedPrefArrayListUtil()
     val list = listOf<String>("갑각류", "견과", "달걀", "땅콩", "밀", "생선", "우유", "조개", "콩")
     val allergyList = ArrayList<String>()
@@ -19,24 +24,41 @@ class RegisterAllergyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterAllergyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        tts2 = TextToSpeech(this, TextToSpeech.OnInitListener {
+            @Override
+            fun onInit(status: Int){
+                if(status != ERROR){
+                    tts2.language = Locale.KOREA
+                }
+            }
+        })
 
         init()
     }
 
     private fun init() {
+        // TODO: 맨 처음 멘트 안나오는거 수정해야함
+        tts2.speak("나는 " + list[cnt] + " 알레르기가 있다.", TextToSpeech.QUEUE_FLUSH, null);
         binding.tvAllergyAType.text = list[cnt]
+
 
         binding.btnAllergyANo.setOnClickListener {
             Log.d(TAG, "init: cnt = $cnt")
             if(++cnt >= list.size) save()
-            else binding.tvAllergyAType.text = list[cnt]
+            else {
+                binding.tvAllergyAType.text = list[cnt]
+                tts2.speak("나는 " + list[cnt] + " 알레르기가 있다.", TextToSpeech.QUEUE_FLUSH, null);
+            }
         }
         binding.btnAllergyAYes.setOnClickListener {
             allergyList.add(list[cnt])
 
             Log.d(TAG, "init: cnt = $cnt")
             if(++cnt >= list.size) save()
-            else binding.tvAllergyAType.text = list[cnt]
+            else {
+                binding.tvAllergyAType.text = list[cnt]
+                tts2.speak("나는 " + list[cnt] + " 알레르기가 있다.", TextToSpeech.QUEUE_FLUSH, null);
+            }
         }
 
     }
@@ -44,6 +66,7 @@ class RegisterAllergyActivity : AppCompatActivity() {
     private fun save(){
         sharePrefArrayListUtil.setStringArrayPref(this, "allergies", allergyList)
         Toast.makeText(this, "알레르기 정보 등록이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+        tts2.speak("알레르기 정보 등록이 완료되었습니다.", TextToSpeech.QUEUE_FLUSH, null)
         finish()
     }
 }
