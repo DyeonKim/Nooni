2fd641646ce16fe40f9010be6db9075854b0e7b9
SuhyungLee <eel0511@gmail.com>
2022-03-17 11:06:35 +0900

    [S06P22D205-57]feat : ✨ PlayMediaUtil stop
    - mp가 메모리누수를 일으킨다고하여 꼭 해제해줘야한다고 해서 stop 메소드 생성

--- Nooni/app/src/main/java/com/ssafy/nooni/util/PlayMediaUtil.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/util/PlayMediaUtil.kt
@@ -8,19 +8,27 @@ import java.net.URLDecoder
 import java.net.URLEncoder
 
 class PlayMediaUtil {
-    private var mediaPlayer: MediaPlayer = MediaPlayer()
+    private var mediaPlayer: MediaPlayer? = MediaPlayer()
     fun start(url: String) {
         try {
             if (mediaPlayer != null) {
-                mediaPlayer.stop()
+                mediaPlayer!!.stop()
             }
             mediaPlayer = MediaPlayer()
-            mediaPlayer.setDataSource(URLDecoder.decode(url, "UTF-8"))
-            mediaPlayer.prepare()
-            mediaPlayer.start()
+            mediaPlayer!!.setDataSource(URLDecoder.decode(url, "UTF-8"))
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
             Log.d("aaaaa", "start: 22222222")
         } catch (e: Exception) {
             Log.d("aaaaa", "start: 11111111")
         }
+    }
+    private fun stop(){
+        if (mediaPlayer!!.isPlaying()) {
+            mediaPlayer!!.stop();
+        }
+        mediaPlayer!!.reset();
+        mediaPlayer!!.release();
+        mediaPlayer = null;
     }
 }
