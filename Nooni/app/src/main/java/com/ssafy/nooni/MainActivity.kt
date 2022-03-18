6e5ccb8a7a215a7b7e267edc5bf422768993c055
SuhyungLee <eel0511@gmail.com>
2022-03-18 12:43:08 +0900

    [S06P22D205-73]  refact : :zap: ViewModel사용위해 리스너를 액티비티로 옮김

--- Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ssafy.nooni
 
 import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.viewpager2.widget.ViewPager2
+import com.ssafy.nooni.Viewmodel.SttViewModel
 import com.ssafy.nooni.adapter.ViewpagerFragmentAdapter
 import com.ssafy.nooni.databinding.ActivityMainBinding
 import com.ssafy.nooni.util.PermissionUtil
@@ -14,7 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     lateinit var permissionUtil: PermissionUtil
-
+    private var sttUtil = SttUtil()
+    private val sttViewModel:SttViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,6 +31,20 @@ class MainActivity : AppCompatActivity() {
             override fun run() {
                 init()
             }
+        }
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,packageName)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR")
+
+        val mRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        mRecognizer.setRecognitionListener(sttlistener)
+        mRecognizer.startListening(intent)
+
+        sttViewModel.stt.observe(this){
+            Log.d("tst5", "onCreate:1111 "+sttViewModel.stt.value)
+            val mRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+            mRecognizer.setRecognitionListener(sttlistener)
+            mRecognizer.startListening(intent)
         }
     }
 
@@ -50,6 +70,66 @@ class MainActivity : AppCompatActivity() {
             permissionUtil.requestPermissions()
         } else {
             init()
+        }
+    }
+    val sttlistener: RecognitionListener = object : RecognitionListener {
+        override fun onReadyForSpeech(params: Bundle) {
+            // 말하기 시작할 준비가되면 호출
+            Log.d("tst5", "시작")
+        }
+
+        override fun onBeginningOfSpeech() {
+            // 말하기 시작했을 때 호출
+        }
+
+        override fun onRmsChanged(rmsdB: Float) {
+            // 입력받는 소리의 크기를 알려줌
+        }
+
+        override fun onBufferReceived(buffer: ByteArray) {
+            // 말을 시작하고 인식이 된 단어를 buffer에 담음
+        }
+
+        override fun onEndOfSpeech() {
+            Log.d("tst5", "중지")
+        }
+
+        override fun onError(error: Int) {
+            // 네트워크 또는 인식 오류가 발생했을 때 호출
+            val message: String
+            message = when (error) {
+                SpeechRecognizer.ERROR_AUDIO -> "오디오 에러"
+                SpeechRecognizer.ERROR_CLIENT -> "클라이언트 에러"
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "퍼미션 없음"
+                SpeechRecognizer.ERROR_NETWORK -> "네트워크 에러"
+                SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "네트웍 타임아웃"
+                SpeechRecognizer.ERROR_NO_MATCH -> "찾을 수 없음"
+                SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RECOGNIZER 가 바쁨"
+                SpeechRecognizer.ERROR_SERVER -> "서버가 이상함"
+                SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "말하는 시간초과"
+                else -> "알 수 없는 오류임"
+            }
+            Log.d("tst5", "onError: $message")
+        }
+
+        override fun onResults(results: Bundle) {
+            // 인식 결과가 준비되면 호출
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줌
+            val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            for (i in 0 until matches!!.size) {
+
+                //textView.setText(matches!![i])
+            }
+            sttViewModel.setStt(matches)
+            Log.d("tst5", "onError: $matches")
+        }
+
+        override fun onPartialResults(partialResults: Bundle) {
+            // 부분 인식 결과를 사용할 수 있을 때 호출
+        }
+
+        override fun onEvent(eventType: Int, params: Bundle) {
+            // 향후 이벤트를 추가하기 위해 예약
         }
     }
 }
