e1e38e6d3b7663da0b75bdf5c0da2bc347aa109b
SuhyungLee <eel0511@gmail.com>
2022-03-21 14:25:43 +0900

    S06P22D205-75] feat : ✨ 빅스비처럼 누니야 부르는것 확인

--- Nooni/app/src/main/java/com/ssafy/nooni/Viewmodel/SttViewModel.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/Viewmodel/SttViewModel.kt
@@ -8,7 +8,13 @@ class SttViewModel:ViewModel() {
     private val _stt = MutableLiveData<ArrayList<String>>()
     val stt : LiveData<ArrayList<String>> get() = _stt
 
+    private val _nooni = MutableLiveData<Boolean>()
+    val nooni : LiveData<Boolean> get() = _nooni
+
     fun setStt(strlist:ArrayList<String>){
         _stt.postValue(strlist)
+    }
+    fun setNooni(flag:Boolean){
+        _nooni.postValue(flag)
     }
 }
