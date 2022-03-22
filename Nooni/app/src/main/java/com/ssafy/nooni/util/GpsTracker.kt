8c9aa6af1d4833f0aad9b9d9536938c41a7a60e6
김현수 <hskim9706@gmail.com>
2022-03-22 18:12:39 +0900

    [S06P22D205-86] feat : :sparkles: gps tracker 구현

---
+++ Nooni/app/src/main/java/com/ssafy/nooni/util/GpsTracker.kt
@@ -0,0 +1,132 @@
+package com.ssafy.nooni.util
+
+import android.Manifest
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+
+import android.os.IBinder
+
+import android.os.Bundle
+
+import android.location.LocationManager
+
+import android.content.pm.PackageManager
+
+import androidx.core.content.ContextCompat
+
+import android.location.Location
+
+import android.location.LocationListener
+import android.util.Log
+
+
+class GpsTracker(context: Context) : Service(), LocationListener {
+    private val mContext: Context = context
+    private var location: Location? = null
+    private var latitude = 0.0
+    private var longitude = 0.0
+    protected var locationManager: LocationManager? = null
+
+
+    private fun getLocation(): Location? {
+        try {
+            locationManager = mContext.getSystemService(LOCATION_SERVICE) as LocationManager?
+            val isGPSEnabled = locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            val isNetworkEnabled =
+                locationManager!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+            if (!isGPSEnabled && !isNetworkEnabled) {
+            } else {
+                val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+                    mContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+                    mContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                    hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+                ) {
+                } else return null
+                if (isNetworkEnabled) {
+                    locationManager!!.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(),
+                        this
+                    )
+                    if (locationManager != null) {
+                        location =
+                            locationManager!!.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                        if (location != null) {
+                            latitude = location!!.getLatitude()
+                            longitude = location!!.getLongitude()
+                        }
+                    }
+                }
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager!!.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(),
+                            this
+                        )
+                        if (locationManager != null) {
+                            location =
+                                locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                            if (location != null) {
+                                latitude = location!!.getLatitude()
+                                longitude = location!!.getLongitude()
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (e: java.lang.Exception) {
+            Log.d("@@@", "" + e.toString())
+        }
+        return location
+    }
+
+    fun getLatitude(): Double {
+        if (location != null) {
+            latitude = location!!.getLatitude()
+        }
+        return latitude
+    }
+
+    fun getLongitude(): Double {
+        if (location != null) {
+            longitude = location!!.getLongitude()
+        }
+        return longitude
+    }
+
+    override fun onProviderDisabled(provider: String) {}
+    override fun onProviderEnabled(provider: String) {}
+    override fun onLocationChanged(p0: Location) {
+
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+    override fun onBind(arg0: Intent?): IBinder? {
+        return null
+    }
+
+    fun stopUsingGPS() {
+        if (locationManager != null) {
+            locationManager!!.removeUpdates(this@GpsTracker)
+        }
+    }
+
+    companion object {
+        private const val MIN_DISTANCE_CHANGE_FOR_UPDATES: Long = 10
+        private const val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong()
+    }
+
+    init {
+        getLocation()
+    }
+}
