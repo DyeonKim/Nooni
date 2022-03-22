9fa517d1a44a474bbeb66f8e82a9b717b44ef4ad
김현수 <hskim9706@gmail.com>
2022-03-22 18:14:30 +0900

    [S06P22D205-86] feat : :sparkles: 지도 기능 구현
    - 현 위치 및 주변 편의점 표시
    - 가장 가까운 편의점 찾아 도보 경로 표시

--- Nooni/app/src/main/java/com/ssafy/nooni/MapFragment.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/MapFragment.kt
@@ -1,37 +1,57 @@
 package com.ssafy.nooni
 
-import android.Manifest
 import android.content.Context
-import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.skt.Tmap.TMapGpsManager
 import com.skt.Tmap.TMapView
 import com.ssafy.nooni.databinding.FragmentMapBinding
-import android.location.LocationManager
 
-import androidx.core.app.ActivityCompat
+import com.skt.Tmap.TMapData
+import com.skt.Tmap.poi_item.TMapPOIItem
 
-import android.content.pm.PackageManager
+import com.skt.Tmap.TMapPoint
 
-import android.location.LocationListener
-import android.util.Log
+import android.graphics.Bitmap
+
+import com.skt.Tmap.TMapMarkerItem
+import android.graphics.BitmapFactory
+import com.skt.Tmap.TMapData.*
+
+import com.skt.Tmap.TMapData.FindAroundNamePOIListenerCallback
+
+import android.graphics.Color
+
+import com.ssafy.nooni.util.GpsTracker
+
+import com.skt.Tmap.TMapCircle
+import java.lang.Exception
 
 
-class MapFragment : Fragment(), TMapGpsManager.onLocationChangedCallback {
+class MapFragment : Fragment() {
     private lateinit var binding: FragmentMapBinding
     private lateinit var mainActivity: MainActivity
 
     private val key = BuildConfig.TMAP_API_KEY
 
     private lateinit var tMapView: TMapView
-    private lateinit var tMapGpsManager: TMapGpsManager
+    private var gpsTracker: GpsTracker? = null
 
-    var isFirst = true
+    var latitude = 0.0
+    var longitude = 0.0
+
+    //원 크기 지정
+    var minRadius = 500.0 // 500 meters
+    var radius100 = 100.0 // 100 meters
+
+    //마커 핀 이미지
+    var markerItem1: TMapMarkerItem? = null
+    var bitmap: Bitmap? = null
+    var bitmap2:Bitmap? = null
+    private var tMapPoint: TMapPoint? = null  //현재 위치 포인트
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -50,21 +70,26 @@ class MapFragment : Fragment(), TMapGpsManager.onLocationChangedCallback {
         super.onResume()
         mainActivity.findViewById<TextView>(R.id.tv_title).text = "길안내"
         mainActivity.ttsSpeak("길 안내 화면입니다.")
+
+        tMapView.onResume()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bitmap = BitmapFactory.decodeResource(requireContext().resources, R.drawable.ic_baseline_location_on_24)
+        bitmap2 = BitmapFactory.decodeResource(requireContext().resources, R.drawable.icon_marker)
+        bitmap2 = Bitmap.createScaledBitmap(bitmap2!!, 50, 50, false)
+
         setAuth()
         setMap()
-        setGps()
+        findCVS()
 
+        binding.llTmap.addView(tMapView)
     }
 
     private fun setAuth(){
         tMapView = TMapView(requireContext())
-        tMapGpsManager = TMapGpsManager(requireActivity())
-
         tMapView.setSKTMapApiKey(key)
     }
 
@@ -73,62 +98,122 @@ class MapFragment : Fragment(), TMapGpsManager.onLocationChangedCallback {
         tMapView.setIconVisibility(true)
         tMapView.mapType = TMapView.MAPTYPE_STANDARD
         tMapView.setLanguage(TMapView.LANGUAGE_KOREAN)
+//        tMapView.setCompassMode(true);
+        //tMapView.setTrackingMode(true);
+        tMapView.setSightVisible(true)
 
-        binding.llTmap.addView(tMapView)
     }
 
-    private fun setGps() {
-        val lm = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                1
-            )
-        }
-        lm.requestLocationUpdates(
-            LocationManager.GPS_PROVIDER,  // 등록할 위치제공자(실내에선 NETWORK_PROVIDER 권장)
-            1000, 1f,  // 통지사이의 최소 변경거리 (m)
-            mLocationListener
-        )
+    //원 그리기
+    private fun drawCircle() {
+        val tMapPoint = TMapPoint(latitude, longitude)
+
+        //위치 옮기고 원을 다시 그려주기 전에 이전의 원 지우기
+        tMapView.removeAllTMapCircle()
+
+        //500m 원
+        val tMapCircle = TMapCircle()
+        tMapCircle.centerPoint = tMapPoint
+        tMapCircle.radius = minRadius
+        tMapCircle.circleWidth = 1f
+        tMapCircle.lineColor = Color.GRAY
+        tMapCircle.areaColor = Color.GRAY
+        tMapCircle.areaAlpha = 50
+        tMapView.addTMapCircle("circle1", tMapCircle)
+
+        //100m 원
+        val tMapCircleSmall = TMapCircle()
+        tMapCircleSmall.centerPoint = tMapPoint
+        tMapCircleSmall.radius = radius100
+        tMapCircleSmall.circleWidth = 1f
+        tMapCircle.lineColor = Color.LTGRAY
+        tMapCircleSmall.areaColor = Color.LTGRAY
+        tMapCircleSmall.areaAlpha = 50
+        tMapView.addTMapCircle("circle2", tMapCircleSmall)
     }
 
-    override fun onLocationChange(location: Location) {
-        tMapView.setLocationPoint(location.longitude, location.latitude)
-        tMapView.setCenterPoint(location.longitude, location.latitude)
-    }
+    //편의점 찾기
+    private fun findCVS() {
+        val tMapData = TMapData()
 
-    private val mLocationListener: LocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
+        //지도를 내 현재위치로, 지도의 센터포인트를 내 현재위치로
+        gpsTracker = GpsTracker(requireContext())
+        latitude = gpsTracker!!.getLatitude()
+        longitude = gpsTracker!!.getLongitude()
+        tMapPoint = TMapPoint(latitude, longitude)
+        markerItem1 = TMapMarkerItem()
+        markerItem1!!.setIcon(bitmap) //마커핀 이미지 연결
+        markerItem1!!.setPosition(0.5f, 1.0f) //마커핀 위치 조정
+        markerItem1!!.setTMapPoint(tMapPoint) //마커핀 위치 연결
+        tMapView.addMarkerItem("현재 나의 위치", markerItem1)
 
-            //현재위치의 좌표를 알수있는 부분
-            if (location != null) {
-                val latitude = location.latitude
-                val longitude = location.longitude
-                if (isFirst) {
-                    tMapView.setCenterPoint(longitude, latitude)
-                    isFirst = false
+        //지도 중심 좌표 조정
+        tMapView.setCenterPoint(longitude, latitude, false)
+        tMapView.setLocationPoint(longitude, latitude)
+
+        //500m, 100m 원 그리는 메소드
+        drawCircle()
+
+        //"편의점" 키워드로 검색
+        tMapData.findAroundNamePOI(tMapPoint, "편의점",
+            FindAroundNamePOIListenerCallback { poiItem ->
+                if (poiItem == null) return@FindAroundNamePOIListenerCallback
+                val tMapPointStart = TMapPoint(latitude, longitude) // 출발지
+                tMapView.removeAllMarkerItem()
+                var minDistance = Double.POSITIVE_INFINITY
+                var minDistancePoint: TMapPoint? = null
+
+
+                //텍스트 뷰에 넣을 편의점 정보
+                var itemInfo = TMapPOIItem()
+                for (i in 0 until poiItem.size) {
+                    val item = poiItem[i] as TMapPOIItem
+                    val distance = item.getDistance(tMapPointStart)
+
+
+                    //500m 안에 있는 편의점들 마커핀으로 표시
+                    if (distance < minRadius) {
+                        val markerItem = TMapMarkerItem()
+                        markerItem.icon = bitmap2
+                        markerItem.setPosition(0.5f, 1.0f)
+                        markerItem.tMapPoint = item.poiPoint // 마커의 좌표 지정
+                        markerItem.name = item.poiName.toString()
+                        markerItem.canShowCallout = true
+                        markerItem.calloutTitle = item.poiName.toString()
+                        tMapView.addMarkerItem("poi_$i", markerItem)
+                        val tMapPointEnd = item.poiPoint
+                        if (distance < minDistance) {
+                            minDistance = distance
+                            minDistancePoint = tMapPointEnd
+                            itemInfo = poiItem[i] as TMapPOIItem
+                        }
+                    }
                 }
-                tMapView.setLocationPoint(longitude, latitude)
-                Log.d("TmapTest", "$longitude,$latitude")
-            }
-        }
 
-        override fun onProviderDisabled(provider: String) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+                try {
+                    val minDistancePolyLine = TMapData().findPathDataWithType(
+                        TMapPathType.PEDESTRIAN_PATH,
+                        tMapPointStart,
+                        minDistancePoint
+                    )
+
+                    // //내 위치 마커
+                    markerItem1!!.setTMapPoint(tMapPoint)
+                    tMapView.addMarkerItem("현재 나의 위치", markerItem1)
+
+                    //가까운 거리 인식 되면 선 그리고 편의점 정보 텍스트뷰에 올리기
+                    if (minDistancePolyLine != null) {
+                        minDistancePolyLine.lineColor = R.color.nooni
+                        minDistancePolyLine.outLineColor = R.color.nooni
+                        minDistancePolyLine.lineWidth = 5f
+                        tMapView.addTMapPolyLine("minDistanceLine", minDistancePolyLine)
+                        binding.textView5.text = itemInfo.poiName
+
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            })
     }
-
-
 
 }
