f854351977576e4e288277e0ca31113755aa7eea
KimDoyeon <vlxjdus@gmail.com>
2022-03-23 13:31:11 +0900

    [S06P22D205-41]feat : :sparkles: 성분결과를 못 찾은 경우 처리, 문자열 정리 조건문 수정

--- Nooni/app/src/main/java/com/ssafy/nooni/repository/PrdInfoRepository.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/repository/PrdInfoRepository.kt
@@ -13,6 +13,8 @@ class PrdInfoRepository {
     val _allergenList = MutableLiveData<List<String>>()
 
     suspend fun getAllergen(prdNo: String) {
+        var list = listOf("알 수 없음")
+
         try {
             val response = withContext(Dispatchers.IO) {
                 prdInfoService.getPrdInfo(prdNo)
@@ -22,17 +24,18 @@ class PrdInfoRepository {
                     val data = response.body()!!
                     if (data.totalCount.toLong() > 0) {
                         var strAllergen = data.list[0].allergy
-                        if (strAllergen.substring(strAllergen.length - 3) == " 함유")
+                        if (strAllergen.length > 3 && strAllergen.substring(strAllergen.length - 3) == " 함유")
                             strAllergen = strAllergen.substring(0 until strAllergen.length - 3)
-                        _allergenList.postValue(strAllergen.split(","))
+                        list = strAllergen.split(",")
                     }
                 }
             } else {
                 Log.d(TAG, "onError: Error Code ${response.code()}")
             }
-
         } catch (e: Exception) {
             Log.d(TAG, e.message ?: "onFailure")
+        } finally {
+            _allergenList.postValue(list)
         }
     }
 }
