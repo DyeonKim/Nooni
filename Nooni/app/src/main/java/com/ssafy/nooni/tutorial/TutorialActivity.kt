05c1c5e66661869cd27cfe10e754685da69241ee
송경희 <kyunghee794613@gmail.com>
2022-03-25 14:10:48 +0900

    [S06P22D205-91] feat: :sparkles: ViewPager Adapter 연결

--- Nooni/app/src/main/java/com/ssafy/nooni/tutorial/TutorialActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/tutorial/TutorialActivity.kt
@@ -2,11 +2,33 @@ package com.ssafy.nooni.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.ssafy.nooni.R
+import com.ssafy.nooni.databinding.ActivityMainBinding
+import com.ssafy.nooni.databinding.ActivityTutorialBinding
 
 class TutorialActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTutorialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tutorial)
+        binding = ActivityTutorialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val pagerAdapter = TutorialViewPagerAdapter(this)
+        // 3개의 Fragment Add
+        pagerAdapter.addFragment(TutorialOneFragment())
+        pagerAdapter.addFragment(TutorialTwoFragment())
+        pagerAdapter.addFragment(TutorialThreeFragment())
+        // Adapter
+        binding.viewPager.adapter = pagerAdapter
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+            }
+        })
+
     }
 }
