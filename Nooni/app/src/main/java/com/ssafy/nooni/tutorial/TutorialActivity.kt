e56aaa0dfbf47809b892481abf1b550b764a9841
송경희 <kyunghee794613@gmail.com>
2022-03-28 15:33:54 +0900

    [S06P22D205-92] feat: :sparkles: 튜토리얼 첫 화면 TTS 추가

--- Nooni/app/src/main/java/com/ssafy/nooni/tutorial/TutorialActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/tutorial/TutorialActivity.kt
@@ -3,21 +3,25 @@ package com.ssafy.nooni.tutorial
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.preference.PreferenceManager
 import androidx.viewpager2.widget.ViewPager2
 import com.ssafy.nooni.MainActivity
 import com.ssafy.nooni.R
+import com.ssafy.nooni.adapter.ViewpagerFragmentAdapter
 import com.ssafy.nooni.databinding.ActivityMainBinding
 import com.ssafy.nooni.databinding.ActivityTutorialBinding
+import java.util.*
 
 class TutorialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTutorialBinding
+    lateinit var tts: TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTutorialBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        init()
         val pagerAdapter = TutorialViewPagerAdapter(this)
         // 4개의 Fragment Add
         pagerAdapter.addFragment(TutorialOneFragment())
@@ -71,6 +75,25 @@ class TutorialActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun init() {
+        tts = TextToSpeech(this, TextToSpeech.OnInitListener {
+            @Override
+            fun onInit(status: Int) {
+                if (status != TextToSpeech.ERROR) {
+                    tts.language = Locale.KOREA
+                }
+            }
+        })
+    }
+
+    fun ttsSpeak(text: String) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     override fun onDestroy() {
--- Nooni/app/src/main/java/com/ssafy/nooni/tutorial/TutorialOneFragment.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/tutorial/TutorialOneFragment.kt
@@ -1,17 +1,20 @@
 package com.ssafy.nooni.tutorial
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.ssafy.nooni.MainActivity
 import com.ssafy.nooni.R
-import com.ssafy.nooni.databinding.FragmentContactBinding
 import com.ssafy.nooni.databinding.FragmentTutorialOneBinding
 
 
 class TutorialOneFragment : Fragment() {
     private lateinit var binding: FragmentTutorialOneBinding
+    private lateinit var tutorialActivity: TutorialActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +22,16 @@ class TutorialOneFragment : Fragment() {
     ): View? {
         binding = FragmentTutorialOneBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        tutorialActivity = context as TutorialActivity
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tutorialActivity.ttsSpeak(resources.getString(R.string.tutorial0))
     }
 
 }
