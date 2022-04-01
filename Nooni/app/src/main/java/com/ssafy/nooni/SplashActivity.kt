f718c0e3af0c2c07b9039e1798f2e04ebf1215f7
김현수 <hskim9706@gmail.com>
2022-04-01 11:14:14 +0900

    [S06P22D205-117] style : :art: splash 수정

--- Nooni/app/src/main/java/com/ssafy/nooni/SplashActivity.kt
+++ Nooni/app/src/main/java/com/ssafy/nooni/SplashActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.ImageView
 import androidx.preference.PreferenceManager
+import com.bumptech.glide.Glide
 import com.ssafy.nooni.tutorial.TutorialActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -12,10 +15,13 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val gifLogo: ImageView = findViewById<View>(R.id.logo) as ImageView
+        Glide.with(this).load(R.drawable.logo_gif).into(gifLogo)
+
         startLoading()
     }
 
-    fun startLoading(){
+    private fun startLoading(){
         val handler = Handler()
         handler.postDelayed(Runnable {
             PreferenceManager.getDefaultSharedPreferences(this).apply {
