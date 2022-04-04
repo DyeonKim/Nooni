d2dfefbcce95fe779a0e656b73626c05669e542c
송경희 <kyunghee794613@gmail.com>
2022-04-04 17:29:00 +0900

    index.html 추가

---
+++ server/nooniPjt/src/main/java/com/ssafy/nooni/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.ssafy.nooni.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	
+}
