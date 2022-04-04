feb6d2d923932790355e99370f8509707aab1723
송경희 <kyunghee794613@gmail.com>
2022-04-04 16:35:33 +0900

    war로 변경

---
+++ server/nooniPjt/src/main/java/com/ssafy/nooni/NooniPjtApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.nooni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NooniPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NooniPjtApplication.class, args);
+	}
+
+}
