a5e2781ad5c814a03282ac26906439b32861f011
송경희 <kyunghee794613@gmail.com>
2022-04-04 18:43:56 +0900

    update WebController

--- server/nooniPjt/src/main/java/com/ssafy/nooni/controller/WebController.java
+++ server/nooniPjt/src/main/java/com/ssafy/nooni/controller/WebController.java
@@ -4,13 +4,16 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,15 +27,22 @@ public class WebController {
 
     @GetMapping(value = "/download")
     public void downloadFile(HttpServletResponse response) throws Exception{
-//        Path root = Paths.get(URI.create(path + fileName));
+
         // 프로젝트 폴더의 temp.jpg 파일 로드
         String fileName = "nooni_v1.0.0.apk";
-        Resource resource = resourceLoader.getResource("classpath:/"+ fileName);	
-		File file = resource.getFile();
-  
+//        Resource resource = resourceLoader.getResource("classpath:/"+ fileName);	
+//		File file = resource.getFile();
+        ClassPathResource classPathResource = new ClassPathResource(fileName);
+        InputStream inputStream = classPathResource.getInputStream();
+        File file = File.createTempFile("nooni_v1.0.0", ".apk");
         // 클라이언트에서 아래의 이름으로 파일이 받아진다.
-        String newFileName = "new_nooni_v1.0.0.apk";
-
+        String newFileName = "n_nooni_v1.0.0.apk";
+        try {
+        	FileUtils.copyInputStreamToFile(inputStream, file);
+        } finally {
+        	IOUtils.closeQuietly(inputStream);
+        }
+        
         try (
                 FileInputStream fis = new FileInputStream(file);
                 BufferedInputStream bis = new BufferedInputStream(fis);
